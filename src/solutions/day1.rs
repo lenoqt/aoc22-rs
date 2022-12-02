90c4b5fc89b0fc7a5667b539c16ea223726f8654
Gustavo Barrios <gustavoantonio51@gmail.com>
2022-12-02 12:53:13 +0900

    solution to day 1

---
+++ src/solutions/day1.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+
+#[derive(Debug, Clone)]
+pub struct Elf
+{
+    pub elf_id: i32,
+    pub calories: i32,
+}
+
+impl Elf {
+    pub fn new() -> Self {
+        Elf { elf_id: 0, calories: 0 }
+    }
+
+    pub fn set_zero_calories(&mut self) {
+        self.calories = 0;
+    }
+
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn solution(input_path: &str) -> i32 {
+    let elf = &mut Elf::new();
+    let max_calories=&mut 0;
+    if let Ok(lines) = read_lines(input_path) {
+        for line in lines {
+            if let Ok(calories) = line.unwrap().trim().parse::<i32>() {
+                elf.calories += calories;
+            } else {
+                if elf.calories > *max_calories {
+                    *max_calories = elf.calories;
+                }
+                elf.elf_id = elf.elf_id + 1;
+                elf.set_zero_calories();
+                continue;
+            }
+        }
+    }
+    *max_calories
+}
+
+
+#[test]
+fn test_solution() {
+    let input_path = "./input_files/input_example1.txt";
+    let max_calories = solution(input_path);
+    assert_eq!(max_calories, 24000);
+}
