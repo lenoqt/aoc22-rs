a91da0704a5c50d937c1c019fc50cf56bc7e211d
Gustavo Barrios <gustavoantonio51@gmail.com>
2022-12-02 12:51:18 +0900

    solution to day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use aoc22_rs::solutions::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let day1_solution = solution("./input_files/input_day1.txt");
+    println!("Max: {}", day1_solution);
 }
